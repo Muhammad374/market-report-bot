292785451099cfde4f9c9a01ea16844c660a8f74
Muhammad374 <mohammadpdn1385@gmail.com>
2026-07-17 21:46:01 +0330

    Integrate Telegram bot to send market updates

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Market Bot Started")
+import os
+from telegram import Bot
+
+TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL = os.environ["CHANNEL_ID"]
+
+bot = Bot(token=TOKEN)
+
+message = """
+📊 گزارش تست بازار
+
+🟡 طلا: در حال دریافت...
+₿ بیت‌کوین: در حال دریافت...
+
+⏱ بروزرسانی خودکار فعال شد
+"""
+
+bot.send_message(
+    chat_id=CHANNEL,
+    text=message
+)
+
+print("Message sent successfully")
